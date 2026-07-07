bd1a0b9d6bea1343122d327fe330cf5279428a6d
yuki_cjm <yuki_cjm@sjtu.edu.cn>
2026-07-07 21:15:28 +0800

    feat: Add a sphere.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,20 @@ use crate::color::{Color, get_color};
 use crate::ray::Ray;
 use crate::vec3::{Point3, Vec3};
 
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+    let oc = *center - r.orig;
+    let a = Vec3::dot(&r.dir, &r.dir);
+    let b = -2.0 * Vec3::dot(&r.dir, &oc);
+    let c = Vec3::dot(&oc, &oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant >= 0.0
+}
+
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction = r.dir.unit_vector();
     let a = 0.5 * (unit_direction.y + 1.0);
     (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
