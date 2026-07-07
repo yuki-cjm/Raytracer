9f3660480d9597ea079492d8c1fa3ad8da732ac3
yuki_cjm <yuki_cjm@sjtu.edu.cn>
2026-07-07 20:46:39 +0800

    feat: Complete 'Rays, a Simple Camera, and Background'.

---
+++ src/ray.rs
@@ -0,0 +1,21 @@
+use crate::vec3::{Point3, Vec3};
+
+pub struct Ray {
+    #[allow(dead_code)]
+    pub orig: Point3,
+    pub dir: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: &Point3, direction: &Vec3) -> Self {
+        Self {
+            orig: *origin,
+            dir: *direction,
+        }
+    }
+    
+    #[allow(dead_code)]
+    pub fn at(&self, t: f64) -> Point3 {
+        self.orig + self.dir * t
+    }
+}
