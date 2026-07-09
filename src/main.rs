9918ce884e12352a4d83a296d2c847aff0d412d4
yuki_cjm <yuki_cjm@sjtu.edu.cn>
2026-07-10 00:06:40 +0800

    feat: Final render.

--- src/main.rs
+++ src/main.rs
@@ -15,57 +15,84 @@ use crate::hittable_list::HittableList;
 #[allow(unused_imports)]
 use crate::material::{Dielectric, Lambertian, Metal};
 #[allow(unused_imports)]
-use crate::rtweekend::PI;
+use crate::rtweekend::{PI, random_double, random_range};
 use crate::sphere::Sphere;
-use crate::vec3::Point3;
+#[allow(unused_imports)]
+use crate::vec3::{Point3, Vec3};
 use std::rc::Rc;
 
 fn main() {
     let mut world = HittableList::new();
 
-    let material_ground = Rc::new(Lambertian::new(&Color::new(0.8, 0.8, 0.0)));
-    let material_center = Rc::new(Lambertian::new(&Color::new(0.1, 0.2, 0.5)));
-    let material_left = Rc::new(Dielectric::new(1.50));
-    let material_bubble = Rc::new(Dielectric::new(1.00 / 1.50));
-    let material_right = Rc::new(Metal::new(&Color::new(0.8, 0.6, 0.2), 1.0));
+    let ground_material = Rc::new(Lambertian::new(&Color::new(0.5, 0.5, 0.5)));
+    world.add(Box::new(Sphere::new(
+        &Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_material,
+    )));
 
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = random_double();
+            let center = Point3::new(
+                a as f64 + 0.9 * random_double(),
+                0.2,
+                b as f64 + 0.9 * random_double(),
+            );
+
+            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                if choose_mat < 0.8 {
+                    // diffuse
+                    let albedo = Color::random_vec3() * Color::random_vec3();
+                    let sphere_material = Rc::new(Lambertian::new(&albedo));
+                    world.add(Box::new(Sphere::new(&center, 0.2, sphere_material)));
+                } else if choose_mat < 0.95 {
+                    // metal
+                    let albedo = Color::random_vec3_range(0.5, 1.0);
+                    let fuzz = random_range(0.0, 0.5);
+                    let sphere_material = Rc::new(Metal::new(&albedo, fuzz));
+                    world.add(Box::new(Sphere::new(&center, 0.2, sphere_material)));
+                } else {
+                    // glass
+                    let sphere_material = Rc::new(Dielectric::new(1.5));
+                    world.add(Box::new(Sphere::new(&center, 0.2, sphere_material)));
+                }
+            }
+        }
+    }
+
+    let material1 = Rc::new(Dielectric::new(1.5));
     world.add(Box::new(Sphere::new(
-        &Point3::new(0.0, -100.5, -1.0),
-        100.0,
-        material_ground,
+        &Point3::new(0.0, 1.0, 0.0),
+        1.0,
+        material1,
     )));
+
+    let material2 = Rc::new(Lambertian::new(&Color::new(0.4, 0.2, 0.1)));
     world.add(Box::new(Sphere::new(
-        &Point3::new(0.0, 0.0, -1.2),
-        0.5,
-        material_center,
+        &Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        material2,
     )));
+
+    let material3 = Rc::new(Metal::new(&Color::new(0.7, 0.6, 0.5), 0.0));
     world.add(Box::new(Sphere::new(
-        &Point3::new(-1.0, 0.0, -1.0),
-        0.5,
-        material_left,
-    )));
-    world.add(Box::new(Sphere::new(
-        &Point3::new(-1.0, 0.0, -1.0),
-        0.4,
-        material_bubble,
-    )));
-    world.add(Box::new(Sphere::new(
-        &Point3::new(1.0, 0.0, -1.0),
-        0.5,
-        material_right,
+        &Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        material3,
     )));
 
     let cam = Camera::new(
         16.0 / 9.0,
-        400,
-        100,
+        1200,
+        500,
         50,
         20.0,
-        &Point3::new(-2.0, 2.0, 1.0),
-        &Point3::new(0.0, 0.0, -1.0),
+        &Point3::new(13.0, 2.0, 3.0),
+        &Point3::new(0.0, 0.0, 0.0),
         &Point3::new(0.0, 1.0, 0.0),
+        0.6,
         10.0,
-        3.4,
     );
     cam.render(&world);
 }
